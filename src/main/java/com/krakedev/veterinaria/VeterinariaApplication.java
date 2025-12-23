baf2c4ad2f6e7aa64dee9a9e32ad4010e8c2ff31
Angel-DGP <angel.patinogd@gmail.com>
2025-12-23 17:03:58 -0500

    Reto 3 - Primer Controller

--- src/main/java/com/krakedev/veterinaria/VeterinariaApplication.java
+++ src/main/java/com/krakedev/veterinaria/VeterinariaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VeterinariaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		SpringApplication.run(VeterinariaApplication.class, args);
 	}
 
