3f7596babb0882cf8eed3f3baa57b0ce336098e4
Angel-DGP <angel.patinogd@gmail.com>
2026-01-03 19:08:22 -0500

    Reto 12 - Service de Mascota

---
+++ src/main/java/com/krakedev/veterinaria/service/impl/MascotaServiceImpl.java
@@ -0,0 +1,62 @@
+package com.krakedev.veterinaria.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.krakedev.veterinaria.entity.Mascota;
+import com.krakedev.veterinaria.repository.MascotaRepository;
+import com.krakedev.veterinaria.service.MascotaService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+
+@Service
+@RequiredArgsConstructor
+public class MascotaServiceImpl implements MascotaService{
+
+    private MascotaRepository mascotaRepository;
+
+    @Override
+    public Mascota registrarMascota(Mascota mascota) {
+        return mascotaRepository.save(mascota);
+    }
+
+    @Override
+    public List<Mascota> listarMascotas() {
+        return mascotaRepository.findAll();
+    }
+
+    @Override
+    public Optional<Mascota> buscarPorNombre(String nombre) {
+        Optional<Mascota> mascotaExistente = mascotaRepository.findByNombre(nombre);
+        return mascotaExistente;
+    }
+
+    @Override
+    public Optional<Mascota> buscarPorId(Long id) {
+        Optional<Mascota> mascotaExistente = mascotaRepository.findById(id);
+        return mascotaExistente;
+    }
+
+    @Override
+    @SneakyThrows
+    public Mascota actualizarMascota(Long id, Mascota mascota) {
+       Mascota mascotaExistente = mascotaRepository.findById(id).orElseThrow(()-> new Exception("No se encontro una mascota con este ID: " + id));
+        mascotaExistente.setEdad(mascota.getEdad());
+        mascotaExistente.setEspecie(mascota.getEspecie());
+        mascotaExistente.setFechaRegistro(mascota.getFechaRegistro());
+        mascotaExistente.setNombre(mascota.getNombre());
+        mascotaExistente.setNombreDueno(mascota.getNombreDueno());
+       return mascotaRepository.save(mascotaExistente);
+    }
+
+    @Override
+    @SneakyThrows
+    public void eliminarMascota(Long id) {
+        Mascota mascotaExistente = mascotaRepository.findById(id).orElseThrow(()-> new Exception("No se encontro una mascota con este ID: " + id));
+        mascotaRepository.delete(mascotaExistente);
+    }
+
+}
